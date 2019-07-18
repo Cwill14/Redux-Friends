47f824874071a91f7d715b5b521d18a7a5a31482
Clark Williams <theclarkwilliams@gmail.com>
2019-07-18 16:42:21 -0600

    rendering list of friends. logout still not working

--- friends/auth-friends/src/components/FriendsList.js
+++ friends/auth-friends/src/components/FriendsList.js
@@ -1,12 +1,32 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import { axiosWithAuth } from '../axiosAuth';
+import Friend from './Friend';
 
 const FriendsList = props => {
+    
+    const [list, setList] = useState([]);
+
+    useEffect(() => {
+        axiosWithAuth()
+            .get('http://localhost:5000/api/friends')
+            .then(res => {
+                console.log(res.data);
+                setList(res.data)
+            })
+            .catch(err => {
+                console.log(err);
+            })
+    }, [])
+    
     return (
         <div>
             <button onClick={() => {
                 localStorage.removeItem('token');
                 props.props.history.push("/");
             }}>Log Out</button>
+            {list.map(eachFriend => {
+                return <Friend friend={eachFriend} key={eachFriend.id} />
+            })}
             FriendsList
         </div>
     );
